1bf0c24d8cb6af507bcc5cf78f2f6fd91938f109
Neo Hsu <locu1214@gmail.com>
2020-04-05 22:52:46 +0800

    學習如何動態載入 component，並設定不會因為切換而被destroy 掉！

---
+++ components/New.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <!-- 用 counter 來驗證每次動態載入 component 時，是否會重新產生 component...預設是會 destroyed! -->
+
+        <button @click="counter++">Increase!</button>
+        <p>{{ counter }}</p>
+    </div>    
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    destroyed() {
+        // 當被動態載入時，沒有被 <keep-alive> 包圍時，每次被切換掉都會被 destroy 掉！
+        console.log('destroyed!');
+    },
+    deactivated() {
+        // component在被切換離開前會執行 deactivated 並於切換回來時呼叫 activated
+        console.log('Deactivated!');
+    },
+    activated() {
+        // component在被切換離開前會執行 deactivated 並於切換回來時呼叫 activated
+        console.log('Activated!');
+    },
+}
+</script>
+
+<style scoped>
+   
+</style>
--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,20 @@
                    <!-- 幫 slot 命名以做到不同內容塞到不同 slot 裡面去 -->
                    <h2 slot="title">{{ titleString }}</h2>
                    <p slot="content">A wonderful quote!</p>
-               </app-quote>
+               </app-quote>                
+
+               以下示範如何動態載入指定的component<br>
+               <button @click="selectedComponent = 'appQuote'">Quote</button>
+               <button @click="selectedComponent = 'appAuthor'">Author</button>
+               <button @click="selectedComponent = 'appNew'">New</button>
+               <p>{{ selectedComponent }}</p>
+               <!-- 用 <component  v-bind:is=""> 來動態指定 component -->
+               <!-- 透過 <keep-alive> 來避免因為動態切換 component 而導致已開啟的 component 被destroy 掉！ -->
+               <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+               </keep-alive>
             </div>
         </div>
     </div>
@@ -16,14 +29,19 @@
 
 <script>
     import QuoteVue from '../components/Quote.vue'
+    import AuthorVue from '../components/Author.vue'
+    import NewVue from '../components/New.vue'
 
     export default {
         components: {
-            appQuote: QuoteVue // 或者寫成 app-quote: QuoteVue 也是可以的
+            appQuote: QuoteVue, // 或者寫成 app-quote: QuoteVue 也是可以的
+            appAuthor: AuthorVue,
+            appNew: NewVue
         },
         data: function() {
             return {
-                titleString: 'Quote!!'
+                titleString: 'Quote!!',
+                selectedComponent: 'appQuote' //若要使用 data 紀錄 component 名稱，務必與所命名相同喔！
             }
         }
     }
