9c596e60e09dbb783f4913500127ae7a8763aaa4
Neo Hsu <locu1214@gmail.com>
2020-04-05 17:57:12 +0800

    使用slot傳遞parent component定義的區塊給 child component

---
+++ components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!-- 使用 slot 來保留 parent component 定義好的 html 內容，此時 CSS也只會吃此 component 定義的 -->
+        <slot></slot>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+
+    h2 {
+        color: red;
+    }
+</style>
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <!-- 想將所定義的內容，原封不動地傳遞給 component 呈現？ 請用slot! -->
+                   <h2>Quote!</h2>
+                   <h2>{{ title }}</h2>
+                   A wonderful quote!
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteVue from '../components/Quote.vue'
+
+    export default {
+        components: {
+            appQuote: QuoteVue // 或者寫成 app-quote: QuoteVue 也是可以的
+        },
+        data: function() {
+            return {
+                title: 'Quote!!'
+            }
+        }
+    }
+</script>
+
+<!-- 發現：這邊定義的CSS並不會影響到要傳給slot的內容 -->
+<!-- slot特性：compile的結果 會吃 parent component定義的，style 會吃 child component 定義的 -->
+<style scoped>
+    h2 {
+        color: blue;
+    }
+</style>
